7d38b6225aabab33e4dcb364fe64500777aa0102
Nami <33597286+Naami5@users.noreply.github.com>
2021-10-26 15:25:24 +0200

    Add project files.

---
+++ Game/Game.cpp
@@ -0,0 +1,14 @@
+#include <algorithm>
+#include <iostream>
+
+int health;
+char name;
+
+int main()
+{
+    std::cout << "Hello!\n";
+    std::cout << "welcome to my game!\n";
+    std::cout << "input your name please! :";
+    std::cin  >> name;
+}
+
